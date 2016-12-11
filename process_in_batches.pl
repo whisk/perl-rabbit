c03c850145b4ddf19a6cda61cf946aec5fd3bfde
Alex Svetkin <svetkin@inventos.ru>
2016-12-11 18:57:54 +0300

    added some comments

--- process_in_batches.pl
+++ process_in_batches.pl
@@ -10,19 +10,26 @@ use Time::HiRes qw(sleep);
 
 our $VERSION = '0.1';
 
+# this is ony for signal handling in our infinite loop
 my $should_stop = 0;
 local $SIG{INT} = \&signal_handler;
 local $SIG{TERM} = \&signal_handler;
 
+# connect to RabbitMQ
 my $rb = ABC::RabbitMQ::Batch->new('localhost', { user => 'guest', password => 'guest' }) or croak;
 
+# do our processing in a infinite loop
 while (!$should_stop) {
+    # process a batch
     my $result = $rb->process({
         channel_id => 1,
         queue_in   => 'test_in',
         queue_out  => 'test_out',
-        handler    => \&msg_handler,
-        batch      => { size => 10, timeout => 2 }
+        handler    => \&msg_handler, # this is processing handler
+        batch      => {
+            size => 10,  # number of messages in a batch
+            timeout => 2 # time to wait if we don't have enough messages to form a complete batch
+        }
     });
     sleep 0.1;
 }
@@ -30,6 +37,9 @@ while (!$should_stop) {
 exit(0);
 ###
 
+# sample handler
+# add "Processed: 1" to all messages
+# emulates random processing failures
 sub msg_handler {
     my $messages = shift;
     my $new_mesages = [];
@@ -57,6 +67,7 @@ sub msg_handler {
     return $new_mesages;
 }
 
+# OS signal handler
 sub signal_handler {
     $should_stop = 1;
     return;
